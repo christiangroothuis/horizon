a1147b62f1c6fd4d84fac62e11d30d4c82e1c96d
NesciO <54905956+RicoClark@users.noreply.github.com>
2021-05-18 16:28:12 +0200

    Update ProvisioningPlan.php

--- src/ProvisioningPlan.php
+++ src/ProvisioningPlan.php
@@ -58,10 +58,14 @@ class ProvisioningPlan
     public static function get($master)
     {        
         $queue = config('horizon.environments.' . (config('horizon.env') ?? config('app.env')) . '.supervisor-1.queue');
-        $extra = \App\Models\Connection\Connection::all()->pluck('queue_name')->toArray();
-        $newQueue = array_merge($queue, $extra);
+        $connectionQueues = \App\Models\Connection\Connection::all()->pluck('queue_name')->toArray();
+        $newQueue = array_merge($queue, $connectionQueues);
+        $analyticsQueues = \App\Models\Emailing\EmailingAnalytics::all()->map(function ($analytic) {
+            return 'analytics_' . $analytic->id . '_' . $analytic->emailing->name;
+        })->toArray();
+        $newQueue = array_merge($newQueue, $analyticsQueues);
         Config::set('horizon.environments.' . (config('horizon.env') ?? config('app.env')) . '.supervisor-1.queue', $newQueue);
-        
+
         return new static($master, config('horizon.environments'), config('horizon.defaults', []));
     }
 
